a221c8e237a5466f7842a105fcaa64cfd20316d8
Matteo Gassend <matteo@Matteos-MacBook-Pro.local>
2025-01-14 19:41:33 +0100

    add: CreateContainer

--- server/docker/containers.go
+++ server/docker/containers.go
@@ -2,12 +2,132 @@ package docker
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"slices"
+	"sync"
+	"time"
 
+	"github.com/charmbracelet/log"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/api/types/volume"
 	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+	"github.com/matfire/pockets/server/utils"
 	sharedv1 "github.com/matfire/pockets/shared/v1"
+
+	getport "github.com/jsumners/go-getport"
 )
+
+func CreateContainer(data *sharedv1.CreateContainerRequest, app *utils.App) (*sharedv1.CreateContainerResponse, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return nil, err
+	}
+	images, err := cli.ImageList(context.Background(), image.ListOptions{
+		All: true,
+	})
+	imgIdx := slices.IndexFunc(images, func(el image.Summary) bool {
+		repoIdx := slices.IndexFunc(el.RepoTags, func(t string) bool {
+			return t == fmt.Sprintf("pockets:%s", data.Version)
+		})
+		return repoIdx != -1
+	})
+	if imgIdx == -1 {
+		return nil, errors.New("could not find requested image")
+	}
+	volume, err := cli.VolumeCreate(context.Background(), volume.CreateOptions{})
+	port, err := getport.GetTcpPortForAddress("0.0.0.0")
+	if err != nil {
+		fmt.Printf("%v", err)
+		return nil, err
+	}
+	res, err := cli.ContainerCreate(context.Background(), &container.Config{
+		Image: fmt.Sprintf("pockets:%s", data.Version),
+		ExposedPorts: nat.PortSet{
+			"8080": struct{}{},
+		},
+		Labels: map[string]string{
+			"pockets": "",
+		},
+	}, &container.HostConfig{
+		Mounts: []mount.Mount{
+			{
+				Type:   mount.TypeVolume,
+				Source: volume.Name,
+				Target: "/pb/pb_data",
+			},
+		},
+		PortBindings: nat.PortMap{
+			// this should be the same as the exposed port
+			"8080": []nat.PortBinding{
+				{
+					// wildcart locahost because docker shenanigans
+					HostIP: "0.0.0.0",
+					//TODO this should be randomly generated
+					HostPort: fmt.Sprintf("%d", port.Port),
+				},
+			},
+		},
+	}, nil, nil, data.Name)
+
+	err = cli.ContainerStart(context.Background(), res.ID, container.StartOptions{})
+	if err != nil {
+		return nil, err
+	}
+	var wg sync.WaitGroup
+	log.Info("Waiting for pocketbase to start properly")
+	time.Sleep(5 * time.Second)
+	adminCmd, err := cli.ContainerExecCreate(context.Background(), res.ID, container.ExecOptions{
+		Cmd:          []string{"/pb/pocketbase", "superuser", "upsert", app.AdminUser, app.AdminPassword},
+		AttachStdout: true,
+		AttachStderr: true,
+	})
+	//CHECK AND BLOCK FOR COMMAND STATUS
+	if err != nil {
+		return nil, err
+	}
+	err = cli.ContainerExecStart(context.Background(), adminCmd.ID, container.ExecStartOptions{
+		Detach: false,
+		Tty:    false,
+	})
+	if err != nil {
+		return nil, err
+	}
+	wg.Add(1)
+	log.Info("Waiting for command to run")
+	go func() {
+		ticker := time.NewTicker(5 * time.Second)
+		for range ticker.C {
+			log.Info("Checking for exec status")
+			status, err := cli.ContainerExecInspect(context.Background(), adminCmd.ID)
+			if err != nil {
+				log.Error(err)
+			}
+			if !status.Running {
+				ticker.Stop()
+				log.Infof("Got status code %d", status.ExitCode)
+				wg.Done()
+			}
+		}
+
+	}()
+	wg.Wait()
+	containerInfo, err := cli.ContainerInspect(context.Background(), res.ID)
+	if err != nil {
+		return nil, err
+	}
+	return &sharedv1.CreateContainerResponse{
+		Container: &sharedv1.Container{
+			Id:     containerInfo.ID,
+			Name:   containerInfo.Name,
+			Status: containerInfo.State.Status,
+		},
+	}, nil
+}
 
 func GetContainers() *sharedv1.GetContainersResponse {
 	cli, err := client.NewClientWithOpts(client.FromEnv)
--- server/rpc/service.go
+++ server/rpc/service.go
@@ -6,6 +6,7 @@ import (
 	"connectrpc.com/connect"
 
 	"github.com/matfire/pockets/server/docker"
+	"github.com/matfire/pockets/server/utils"
 	sharedv1 "github.com/matfire/pockets/shared/v1"
 	"github.com/matfire/pockets/shared/v1/sharedv1connect"
 )
@@ -18,4 +19,10 @@ func (s *PocketsServer) GetContainers(ctx context.Context, req *connect.Request[
 	data := docker.GetContainers()
 	res := connect.NewResponse(data)
 	return res, nil
+}
+
+func (s *PocketsServer) CreateContainer(ctx context.Context, req *connect.Request[sharedv1.CreateContainerRequest]) (*connect.Response[sharedv1.CreateContainerResponse], error) {
+	data, err := docker.CreateContainer(req.Msg, utils.GetConfig())
+	res := connect.NewResponse(data)
+	return res, err
 }
