ef4872e81cd571c4e27d9a11de2db7a1eff4e264
Matteo Gassend <mgassend@gmail.com>
2025-01-18 17:04:48 +0100

    add: switch to starlight

---
+++ docs/astro.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://pockets.matteogassend.com",
+	integrations: [
+		starlight({
+			title: 'Pockets',
+			social: {
+				github: 'https://github.com/matfire/pockets',
+				blueSky: 'https://bsky.app/profile/matteogassend.com'
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Get Started', slug: 'guides/get-started' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
