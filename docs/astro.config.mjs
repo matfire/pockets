d066ee9fce33ff0c650accdda83f970ccb0cd84d
Matteo Gassend <mgassend@gmail.com>
2025-01-18 17:08:02 +0100

    add: cloudflare adapter

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,29 +2,34 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://pockets.matteogassend.com",
-	integrations: [
-		starlight({
-			title: 'Pockets',
-			social: {
-				github: 'https://github.com/matfire/pockets',
-				blueSky: 'https://bsky.app/profile/matteogassend.com'
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Get Started', slug: 'guides/get-started' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  site: "https://pockets.matteogassend.com",
+
+  integrations: [
+      starlight({
+          title: 'Pockets',
+          social: {
+              github: 'https://github.com/matfire/pockets',
+              blueSky: 'https://bsky.app/profile/matteogassend.com'
+          },
+          sidebar: [
+              {
+                  label: 'Guides',
+                  items: [
+                      // Each item here is one entry in the navigation menu.
+                      { label: 'Get Started', slug: 'guides/get-started' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+      }),
 	],
+
+  adapter: cloudflare(),
 });
