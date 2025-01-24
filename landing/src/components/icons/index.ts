e2871674220519a81c106c854b3ef5cc32d122f4
matfire <mgassend@gmail.com>
2025-01-24 11:02:09 +0000

    add: landing (#6)

---
+++ landing/src/components/icons/index.ts
@@ -0,0 +1,10 @@
+import { Database, Layers, Settings, Shield, Zap, ChevronRight} from 'lucide-astro';
+
+export const icons = {
+  Database,
+  Layers,
+  Settings,
+  Shield,
+  Zap,
+  ChevronRight,
+};
